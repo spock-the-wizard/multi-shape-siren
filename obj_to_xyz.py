ded6be3e05f183ba390d040b71fd7b4a8b3ae4b1
spock-the-wizard <jeson@postech.ac.kr>
2021-12-11 22:21:40 +0900

    test data for multi shapes

--- obj_to_xyz.py
+++ obj_to_xyz.py
@@ -1,13 +1,41 @@
+from os import F_LOCK
 import open3d as o3d
+from plyfile import PlyData, PlyElement
+import numpy as np
 
-fn='./data/pointcloud.ply'
-pply=o3d.io.read_triangle_mesh(fn)
-mesh=pply.compute_vertex_normals()
-o3d.io.write_triangle_mesh('./data/tmp/cactus.ply',mesh)
-pply=o3d.io.read_point_cloud('./data/tmp/cactus.ply')
-o3d.io.write_point_cloud("./data/cactus.xyzn",pply)
-print(pply)
+
+
+for i in range(1,5):
+    fn='./data/c{}.ply'.format(i)
+
+    mesh=o3d.io.read_triangle_mesh(fn)
+    mesh.compute_vertex_normals()
+    o3d.io.write_triangle_mesh(fn,mesh)
+    ply=o3d.io.read_point_cloud(fn)
+    o3d.io.write_point_cloud(fn.replace('ply','xyzn'),ply)
 """
+fn='./data/fromdeepsdf.ply'
+
+# plydata=PlyData.read(fn)
+# vertices=plydata['vertex']
+# faces=plydata['face']
+# for i in vertices:
+#     print(i)
+
+# import pdb;pdb.set_trace()
+
+
+
+pply=o3d.io.read_triangle_mesh(fn)
+num_tri=len(np.asarray(pply.triangles))
+pply.remove_triangles_by_index([i for i in range(num_tri)])
+
+#mesh=pply.compute_vertex_normals()
+o3d.io.write_triangle_mesh('./data/tmp/deep.ply',pply)
+pply=o3d.io.read_point_cloud('./data/tmp/deep.ply')
+o3d.io.write_point_cloud("./data/fromdeepsdf.xyzn",pply)
+print(pply)
+
 fn='./data/obj/chair.obj'
 instance=fn.split('/')[-1].split('.')[0]
 mesh=o3d.io.read_triangle_mesh(fn)
