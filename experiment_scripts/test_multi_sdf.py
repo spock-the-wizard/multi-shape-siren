56ecefb4b8d94b8d667471e0ca14b2e85dfde1a2
spock-the-wizard <jeson@postech.ac.kr>
2021-12-12 01:06:46 +0900

    test code for multi sdf

---
+++ experiment_scripts/test_multi_sdf.py
@@ -0,0 +1,58 @@
+'''Test script for experiments in paper Sec. 4.2, Supplement Sec. 3, reconstruction from laplacian.
+'''
+
+# Enable import from parent package
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+import torch
+import modules, utils
+import sdf_meshing
+import configargparse
+
+p = configargparse.ArgumentParser()
+p.add('-c', '--config_filepath', required=False, is_config_file=True, help='Path to config file.')
+
+p.add_argument('--logging_root', type=str, default='./logs', help='root for logging')
+p.add_argument('--experiment_name', type=str, required=True,
+               help='Name of subdirectory in logging_root where summaries and checkpoints will be saved.')
+
+# General training options
+p.add_argument('--checkpoint_path', default=None, help='Checkpoint to trained model.')
+
+p.add_argument('--model_type', type=str, default='sine',
+               help='Options are "sine" (all sine activations) and "mixed" (first layer sine, other layers tanh)')
+p.add_argument('--mode', type=str, default='mlp',
+               help='Options are "mlp" or "nerf"')
+p.add_argument('--resolution', type=int, default=1600)
+p.add_argument('--latvec_cnt',type=int, default=2)
+
+opt = p.parse_args()
+
+
+class MultiSDFDecoder(torch.nn.Module):
+    def __init__(self,latvec_size=1):
+        super().__init__()
+        # Define the model.
+        in_features=3+latvec_size
+        if opt.mode == 'mlp':
+            self.model = modules.SingleBVPNet(type=opt.model_type, final_layer_factor=1, in_features=in_features)
+        elif opt.mode == 'nerf':
+            self.model = modules.SingleBVPNet(type='relu', mode='nerf', final_layer_factor=1, in_features=in_features)
+        self.model.load_state_dict(torch.load(opt.checkpoint_path))
+        self.model.cuda()
+
+    def forward(self, coords):
+        model_in = {'coords': coords}
+        return self.model(model_in)['model_out']
+
+
+sdf_decoder = MultiSDFDecoder()
+
+root_path = os.path.join(opt.logging_root, opt.experiment_name)
+utils.cond_mkdir(root_path)
+
+epoch=opt.checkpoint_path.split('/')[-1].split('.')[0].split('_')[2]
+
+sdf_meshing.create_multi_mesh(sdf_decoder, shapecnt=opt.latvec_cnt,outfile_prefix=os.path.join(root_path, 'epoch_{}_res_{}'.format(epoch,opt.resolution)), N=opt.resolution)
--- sdf_meshing.py
+++ sdf_meshing.py
@@ -9,6 +9,71 @@ import skimage.measure
 import time
 import torch
 
+def create_multi_mesh(
+    decoder, shapecnt, outfile_prefix,N=256, max_batch=64 ** 3, offset=None, scale=None
+):
+
+    decoder.eval()
+
+    # NOTE: the voxel_origin is actually the (bottom, left, down) corner, not the middle
+    voxel_origin = [-1, -1, -1]
+    voxel_size = 2.0 / (N - 1)
+
+    
+    overall_index = torch.arange(0, N ** 3, 1, out=torch.LongTensor())
+
+    for latent in range(shapecnt):
+
+        start = time.time()
+        ply_filename='{}_shape_{}'.format(outfile_prefix,latent)
+        samples = torch.zeros(N ** 3, 4)
+
+        # transform first 3 columns
+        # to be the x, y, z index
+        samples[:, 2] = overall_index % N
+        samples[:, 1] = (overall_index.long() / N) % N
+        samples[:, 0] = ((overall_index.long() / N) / N) % N
+
+        # transform first 3 columns
+        # to be the x, y, z coordinate
+        samples[:, 0] = (samples[:, 0] * voxel_size) + voxel_origin[2]
+        samples[:, 1] = (samples[:, 1] * voxel_size) + voxel_origin[1]
+        samples[:, 2] = (samples[:, 2] * voxel_size) + voxel_origin[0]
+
+        num_samples = N ** 3
+
+        samples.requires_grad = False
+
+        head = 0
+
+        while head < num_samples:
+            print(head)
+            sample_subset = samples[head : min(head + max_batch, num_samples), 0:3].cuda()
+            
+            import pdb; pdb.set_trace
+            samples[head : min(head + max_batch, num_samples), 3] = (
+                decoder(sample_subset)
+                .squeeze()#.squeeze(1)
+                .detach()
+                .cpu()
+            )
+            head += max_batch
+
+        sdf_values = samples[:, 3]
+        sdf_values = sdf_values.reshape(N, N, N)
+
+        end = time.time()
+        print("shape %d sampling takes: %f" % (latent,end - start))
+
+        convert_sdf_samples_to_ply(
+            sdf_values.data.cpu(),
+            voxel_origin,
+            voxel_size,
+            ply_filename + ".ply",
+            offset,
+            scale,
+        )
+
 
 def create_mesh(
     decoder, filename, N=256, max_batch=64 ** 3, offset=None, scale=None
